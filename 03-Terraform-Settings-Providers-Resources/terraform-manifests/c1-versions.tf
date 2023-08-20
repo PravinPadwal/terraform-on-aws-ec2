af442b14dc1e8342961cea23541b41ce455e575c
PravinPadwal <113836217+PravinPadwal@users.noreply.github.com>
2023-08-20 17:59:39 +0530

    practice

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,13 +1,13 @@
 # Terraform Block
-terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.0"
+terraform{
+  required_version = "~> 1.5"
+  required_providers{
+    aws {
+      source = "value"
+      version = ""
     }
-  } 
-}  
+  }
+}
 # Provider Block
 provider "aws" {
   region = "us-east-1"
